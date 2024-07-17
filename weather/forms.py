0028e23b6e81a30d59c8a56837bd4201d3eb2d32
Daniil Chursin <danilcursin@mail.ru>
2024-07-17 15:07:48 +0300

    Добавлена документация для формы

--- weather/forms.py
+++ weather/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 
 class CityForm(forms.Form):
+    """
+    Форма для ввода названия города.
+
+    Attributes:
+        city (CharField): Название города.
+    """
     city = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'autocomplete': 'off'}))
     
